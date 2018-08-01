f6c93f54cb742daf94d2903ac70355f695a8f1ea
Matt Behan <mattbehan@gmail.com>
2018-08-01 13:01:34 -0400

    added more validation tests for following

--- spec/models/following_spec.rb
+++ spec/models/following_spec.rb
@@ -1,12 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Following, type: :model do
-	describe "validations" do
+	describe "basic validations" do
 		it { should validate_presence_of(:followed_user_id) }
-		it { should validate_presence_of(:follower_user_id) }
+		it { should validate_presence_of(:following_user_id) }
+		it { should validate_uniqueness_of(:follower_user_id).scoped_to(:following_user_id)}
 	end
-	describe "associations" do
+	describe "basic associations" do
 		it {should belong_to(:follower)}
 		it {should belong_to(:followed_user)}
+	end
+
+	describe "custom validations" do
+		it "validates that a user cannot follow themselves" do
+			user1 = create(user)
+			following = Following.new(followed_user_id: user1.id, following_user_id: user1.id)
+			following.valid?
+			expect(following.errors).to include ("User cannot follow themselves")
+		end
 	end
 end
