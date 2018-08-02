85a6b2b89846bab23cac9a7fbcfee54dc61ff1d6
Matt Behan <mattbehan@gmail.com>
2018-08-01 20:11:01 -0400

    following models spec passing

--- app/models/following.rb
+++ app/models/following.rb
@@ -1,7 +1,16 @@
 class Following < ApplicationRecord
 	validates :followed_user_id, presence: true
 	validates :following_user_id, presence: true
+	validates :followed_user_id, uniqueness: {scope: :following_user_id}
+	validate :user_cannot_follow_themselves
+
 
 	belongs_to :followed_user, class_name: :User
 	belongs_to :following_user, class_name: :User
+
+	def user_cannot_follow_themselves
+		if followed_user_id == following_user_id
+			errors.add(:users, "cannot follow themselves")
+		end
+	end
 end
--- spec/models/following_spec.rb
+++ spec/models/following_spec.rb
@@ -4,19 +4,23 @@ RSpec.describe Following, type: :model do
 	describe "basic validations" do
 		it { should validate_presence_of(:followed_user_id) }
 		it { should validate_presence_of(:following_user_id) }
-		it { should validate_uniqueness_of(:follower_user_id).scoped_to(:following_user_id)}
+		# necessary to test scoped uniqueness with shoulda matchers - see https://github.com/thoughtbot/shoulda-matchers/issues/745
+		subject(:dup1) { Following.create(followed_user_id: 1, following_user_id: 2) }
+		it "validates the uniqueness of followed_user_id and followed_user_id together to prevent duplicate records" do 
+			should validate_uniqueness_of(:followed_user_id).scoped_to(:following_user_id)
+		end
 	end
 	describe "basic associations" do
-		it {should belong_to(:follower)}
+		it {should belong_to(:following_user)}
 		it {should belong_to(:followed_user)}
 	end
 
 	describe "custom validations" do
 		it "validates that a user cannot follow themselves" do
-			user1 = create(user)
+			user1 = create(:user)
 			following = Following.new(followed_user_id: user1.id, following_user_id: user1.id)
 			following.valid?
-			expect(following.errors).to include ("User cannot follow themselves")
+			expect(following.errors.full_messages.flatten).to include ("Users cannot follow themselves")
 		end
 	end
 end
